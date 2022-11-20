6ea6e60a0da2530bac393b1570d56cdd6983a4de
Ramir Salcedo <ramir101@gmail.com>
2022-11-20 15:01:11 -0500

    logic done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
+import { ContextProvider } from './ContextSockets.js';
+
 
 const root = createRoot(document.querySelector('#root'));
 root.render(
-    <App />
+    <ContextProvider>
+        <App />
+    </ContextProvider>
 );
